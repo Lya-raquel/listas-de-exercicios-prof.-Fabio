3375fbc90abc352bea3f608a516a764624dbb6d1
Lya-raquel <lyaraquelnot189@gmail.com>
2025-05-31 10:55:42 -0300

    Update q11.py

--- lista 03 FOR/q11.py
+++ lista 03 FOR/q11.py
@@ -1,2 +1,17 @@
-# fazer dps
-def 
+# 11. Leia LimiteSuperior e LimiteInferior e escreva todos os números primos entre os limites lidos.
+
+def main():
+    limite_inf = int(input('Limite Inferior: '))
+    limite_sup = int(input('Limite Superior: '))
+
+    for num in range(limite_inf, limite_sup + 1):
+        if num > 1:
+            for i in range (2, int(num ** 0.5) + 1):
+                if num % i == 0:
+                    break
+            else:
+                print(f'{num} é primo.')
+        
+        
+main()      
+      
